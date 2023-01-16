31cdb21edaa667b6b96189b6d9c700a57ef20958
vchangcar <ca.y25@hotmail.com>
2023-01-16 08:20:16 -0500

    index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = require("../app")
+const app = require("./app")
 
-const port = process.env.PORT || 9002
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Server on port ${port}`)
